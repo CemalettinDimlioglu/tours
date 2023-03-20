70156c5f4a58a2d016ffb030803b820c87f00fa0
CMLTTN <cemaldim@gmail.com>
2023-03-20 14:07:34 +0300

    remove

--- setup/src/App.js
+++ setup/src/App.js
@@ -7,6 +7,11 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+  
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
 
   return <h2>Tours Project Setup</h2>
 }
