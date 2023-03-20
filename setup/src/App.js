3da694831d554ee28ac3290326587cbbd1785faa
CMLTTN <cemaldim@gmail.com>
2023-03-20 14:08:17 +0300

    app

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
-  
+
   const removeTour = (id) => {
     const newTours = tours.filter((tour) => tour.id !== id);
     setTours(newTours);
   };
 
-  return <h2>Tours Project Setup</h2>
+  return <h2>Tours Project Setup</h2>;
 }
 
-export default App
+export default App;
